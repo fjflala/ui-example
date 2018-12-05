386c7f5ee3f6179de3c1866f94c4185f8b97830e
Francisco Felix <francisco.felix@ext.privalia.com>
2018-12-05 12:44:35 +0100

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,50 @@
 /**
+ *  Module dependencies
+ */
+const glob = require('glob');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
+/**
+ *  Helpers
+ */
+const entryArray = glob.sync('./src/**/index.js');
+
+const entryObject = entryArray.reduce((acc, item) => {
+  const name = item.replace('/index.js', '').replace('./src/', '');
+  acc[name] = item;
+  return acc;
+}, {});
+
+/**
  *  Webpack Config
  */
 module.exports = {
+  entry: entryObject,
+  output: {
+    filename: 'ui/[name].js'
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: ['babel-loader'],
+          loader: 'babel-loader',
         },
       },
     ],
   },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false
+          }
+        }
+      }),
+      new MinifyPlugin(),
+    ],
+  }
 };
