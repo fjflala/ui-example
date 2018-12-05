4adad0f27b4ab406fdcecfad059b947aa673b4f7
Francisco Felix <francisco.felix@ext.privalia.com>
2018-12-05 11:19:36 +0100

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/**
+ *  Webpack Config
+ */
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: ['babel-loader'],
+        },
+      },
+    ],
+  },
+};
